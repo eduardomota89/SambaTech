762b1bbc69a2a3421491150482b179d24c8f508c
Eduardo Mota <eduardomota.sais@gmail.com>
2016-10-09 21:49:50 -0300

    Teste Samba Tech
    Automação da tela de login da aplicação Samba Vídeos.
    - Verifica se todos os textos da tela de login estão corretos.
    - Verifica se os links levam para as páginas corretas.
    - Verifica autenticação do login.

--- src/com/sambatech/Login/Login.java
+++ src/com/sambatech/Login/Login.java
@@ -1,6 +1,9 @@
 package com.sambatech.Login;
 
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
 import com.sambatech.util.Util;
 
 public class Login extends Util {
@@ -10,10 +13,21 @@ public class Login extends Util {
 
 		propertyChrome();
 		confChrome();
-		login("avaliacao_qa_samba@sambatech.com.br", "");
-		// login("", "12345678");
-		login("avaliacao_qa_samba", "123456789");
-		login("avaliacao_qa_samba@sambatech.com.br", "12345678");
+		preencheLogin("avaliacao_qa_samba@sambatech.com.br", "");
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("password_incorrect")));
+		verificaTexto("password_incorrect", "Email ou senha incorretos. Saiba Mais");
+
+		preencheLogin("", "12345678");
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("password_incorrect")));
+		verificaTexto("password_incorrect", "Email ou senha incorretos. Saiba Mais");
+
+		preencheLogin("avaliacao_qa_samba", "123456789");
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("password_incorrect")));
+		verificaTexto("password_incorrect", "Email ou senha incorretos. Saiba Mais");
+
+		preencheLogin("avaliacao_qa_samba@sambatech.com.br", "12345678");
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("navbar-client-name")));
+		verificaTexto("navbar-client-name", "Avaliação QA");
 		fechar();
 	}
 }
--- src/com/sambatech/util/Util.java
+++ src/com/sambatech/util/Util.java
@@ -6,7 +6,6 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Util {
@@ -23,7 +22,7 @@ public class Util {
 	@Before
 	public void confChrome() {
 		driver.get("http://52.2.166.124:8085/auth/login");
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+//		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 	}
 
@@ -43,23 +42,22 @@ public class Util {
 	 * verifica se mensagem de validação é exibida caso a senha não senha
 	 * preenchida.
 	 */
-	public void login(String usuario, String senha) {
+	public void preencheLogin(String usuario, String senha) {
 
+		driver.findElement(By.id("inputEmail")).clear();
 		driver.findElement(By.id("inputEmail")).sendKeys(usuario);
-		driver.findElement(By.id("inputPassword")).sendKeys(senha);
-		driver.findElement(By.id("login")).click();
 
-		// driver.findElement(By.id("password_incorrect")).getText().equals("Email
-		// ou senha incorretos. Saiba Mais");
-		if (ExpectedConditions.invisibilityOfElementLocated(By.id("password_incorrect")) != null) {
-			driver.findElement(By.id("password_incorrect")).getText().equals("Email ou senha incorretos. Saiba Mais");
-			System.out.println("Mensagem 'Email ou senha incorretos' ===> OK");
-		} else if (ExpectedConditions.invisibilityOfElementLocated(
-				By.xpath("//*[@id='wrap']/div/div[2]/form/ul[2]/li/a/span/text()")) != null) {
-			wait.until(ExpectedConditions.invisibilityOfElementWithText(
-					By.xpath("//*[@id='wrap']/div/div[2]/form/ul[2]/li/a/span/text()"),
-					"Email ou senha incorretos. Saiba Mais"));
-			System.out.println("Login realizado com Sucesso ===> OK");
+		driver.findElement(By.id("inputPassword")).clear();
+		driver.findElement(By.id("inputPassword")).sendKeys(senha);
+
+		driver.findElement(By.id("login")).click();
+	}
+
+	public void verificaTexto(String id, String msg) {
+		if (driver.findElement(By.id(id)).getText().equals(msg)) {
+			System.out.println(msg + " ===> OK");
+		} else {
+			System.out.println(msg + " ===> FAIL");
 		}
 	}
 }
